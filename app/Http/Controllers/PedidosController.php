79478cd6d783cc1d2760d176297efdd08d646e7a
Erik Zerpa <ezerpa@sipecom.com>
2022-07-31 19:18:54 -0400

    aaa

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -722,7 +722,7 @@ class PedidosController extends Controller
 
 
         $pedido_detalle = DB::table('pedidos as a')
-        ->select('a.id','a.monto','a.pedido','a.estatus','a.cantidad','a.total','a.usuario','a.tipopago','a.created_at','a.producto','b.nombre as producto')
+        ->select('a.id','a.monto','a.pedido','a.estatus','a.cantidad','a.cantidad_despachada','a.total','a.usuario','a.tipopago','a.created_at','a.producto','b.nombre as producto')
         ->join('productos as b','b.id','a.producto')
         ->where('a.pedido','=',$id)
         ->where('a.estatus','=',2)
